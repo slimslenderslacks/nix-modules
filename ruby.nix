8db18518a83fea9c337ea0224b47426aee634398
Jim Clark <slimslenderslacks@gmail.com>
2023-09-09 23:11:06 -0700

    Fix ruby devshell expression

--- ruby.nix
+++ ruby.nix
@@ -1,7 +1,10 @@
-{ flake-utils, gitignore, devshell, nix-filter }: { nixpkgs, dir, name, version}:
+{ flake-utils, gitignore, devshell, nix-filter }: { nixpkgs, dir, name, version }:
 flake-utils.lib.eachDefaultSystem (system:
   let
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ devshell.overlays.default ];
+    };
     gems = pkgs.bundlerEnv {
       name = "gems-for-github-linguist";
       gemdir = dir;
@@ -9,7 +12,7 @@ flake-utils.lib.eachDefaultSystem (system:
   in
   {
     # here's a good dev shell with all gems compiled
-    devShells.default = pkgs.mkShell {
+    devShells.default = pkgs.devshell.mkShell {
       packages = with pkgs; [
         bundix
         openssl
@@ -38,15 +41,18 @@ flake-utils.lib.eachDefaultSystem (system:
       };
       buildInputs = [ gems pkgs.ruby ];
       installPhase = ''
-                mkdir -p $out/{bin,share}
-                cp -r ./*.rb $out/share
-                bin=$out/bin/entrypoint
-                cat > $bin <<EOF
-        #!/bin/sh -e
-        exec ${gems}/bin/bundle exec ${pkgs.ruby}/bin/ruby $out/share/main.rb "\$@"
+        mkdir -p $out/{bin,share}
+        cp -r ./*.rb $out/share
+        bin=$out/bin/entrypoint
+        cat > $bin <<EOF
+          #!/bin/sh -e
+          exec ${gems}/bin/bundle exec ${pkgs.ruby}/bin/ruby $out/share/main.rb "\$@"
         EOF
-                chmod +x $bin
+        chmod +x $bin
       '';
+      meta = {
+        mainProgram = "entrypoint";
+      };
     };
 
     # if we just want to distribute a binary from another gem (no ruby code of our own), this would do it!
